f152032a455435830dde25f0524e9935edafce27
brandon <brandon7.7porcel@gmail.com>
2024-10-16 23:49:00 -0300

    fix: snippet mock required keys

--- src/lib/mocks/snippet.mock.ts
+++ src/lib/mocks/snippet.mock.ts
@@ -1,16 +1,20 @@
-import { Snippet } from "../types/snippet";
+import { Snippet, SnippetFile } from "../types/snippet";
 import { randomFileNamesWithExtension } from "../utils/editor_form_utils";
 
-export const defaultNewFile = {
+export const defaultNewFile: SnippetFile = {
   id: crypto.randomUUID(),
   name: randomFileNamesWithExtension[
     Math.floor(Math.random() * randomFileNamesWithExtension.length)
   ],
   code: "",
+  index: 0,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  snippetId: "f945ba55-c1db-497c-a180-086028572837",
 };
 
 export const snippetFormStateDefault: Snippet = {
-  id: "",
+  id: "f945ba55-c1db-497c-a180-086028572837",
   title: "",
   private: true,
   files: [defaultNewFile],
